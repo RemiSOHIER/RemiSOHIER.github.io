982062b072008275857e5d97ae872a7095cd1535
RemiSOHIER <benjamin.sohier55@gmail.com>
2026-06-25 06:29:36 +0200

    -Ajout de la version build pour git pages

---
+++ dist/data/réseaux.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/remi-sohier/",
+        icon: ""
+    },
+    {
+        name: "Github",
+        url: "https://github.com/RemiSOHIER?tab=repositories",
+        icon: ""
+    }
+]
